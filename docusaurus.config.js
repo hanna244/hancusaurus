c422e6abad98b0f0e5ad65b6e7dcbf4b505d58a9
Hanna Kim <shhn0509@gmail.com>
2021-09-26 03:39:41 +0900

    update: config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,8 +3,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: "hanna's 블로그",
+  tagline: '작은 시간이라도 꾸준히!',
   url: 'https://hancusaurus.netlify.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
